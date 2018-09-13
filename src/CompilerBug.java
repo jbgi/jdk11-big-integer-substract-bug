ccfa5b54a06c6274ef0bf12c7c3ac4bbda79cb65
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2018-09-13 20:52:42 +0200

    compiler bug on BigInteger::substract

---
+++ src/CompilerBug.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+public class CompilerBug {
+
+  public static final Optional<BigInteger> oi = Optional.of(BigInteger.TEN).flatMap(i -> {
+    Optional<BigInteger> one =  Optional.of(BigInteger.valueOf(1L));
+    return Optional.of(BigInteger.valueOf(2L)).or(() -> one.map(b -> i.subtract(b)));
+  });
+
+}
